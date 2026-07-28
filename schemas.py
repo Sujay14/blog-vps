75e59f8e62b143d15256b201c85b9611f08ef699
Sujay14 <sujaye14@gmail.com>
2026-07-28 14:47:17 +0530

    addes db and models/schemas

--- schemas.py
+++ schemas.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, EmailStr
 
 class UserBase(BaseModel):
     username: str = Field(min_length=1,max_length=50)
@@ -16,7 +16,12 @@ class UserResponse(UserBase):
     image_file: str | None
     image_path: str
     
+class UserUpdate(BaseModel):
+    username: str | None = Field(default=None, min_length=1,max_length=50)
+    email: EmailStr | None = Field(default=None, max_length=120)
+    image_file:str | None = Field(default=None, min_length=1,max_length=200)
 
+    
 class PostBase(BaseModel):
     title: str = Field(min_length=1,max_length=100)
     content: str=Field(min_length=1)
@@ -25,6 +30,10 @@ class PostBase(BaseModel):
 class PostCreate(PostBase):
     user_id : int  #temp
     
+
+class PostUpdate(BaseModel):
+    title: str | None = Field(default=None,min_length=1,max_length=100)
+    content: str | None =Field(default=None , min_length=1)
 
 class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
